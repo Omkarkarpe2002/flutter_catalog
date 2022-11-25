a722f688c6493a232691a0530d449b0c5f003d9a
Abhishek Jagtap <abhishekjagtap006@gmail.com>
2022-11-25 10:27:20 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 //We are importing this package because we are going to use material library.
 //every program's entry point in the most of languages in the main function.
 //Similary in dart and flutter is void main function it's the entry point to the
@@ -21,17 +22,26 @@ class MyApp extends StatelessWidget {
   //This is the method we are calling again and again when we are refreshing our application.
   //build method contains most of our user interface part.
   Widget build(BuildContext context) {
-    int days = 21;
-    String name = "Codepur";
-    double pi = 3.14;
-    bool is_true = true;
-    num temp = 3.5;
-    var dayOfWeek = "tuesday";
     //We cannot directly use container becuase we are returning an application so we have
     //to return an application like materialapp , cupertinoapp , etc.
     return MaterialApp(
       //We are wrapping container with material library because we are trying to give it the feel of material application
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+          brightness: Brightness.dark, primarySwatch: Colors.blueGrey),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/Login": (context) => LoginPage(),
+        "/home": (context) => HomePage()
+      },
     );
   }
+}
+
+void bringVegetables({int rupees = 100}) {
+  //When we put curlly braces we treat the rupees as optional paramter
+  //Like when we don't especify rupess it will take rupess to default vaule of 100.
+  //But if we specify like (rupess : 50) it will take the value of rupees as 50
 }
